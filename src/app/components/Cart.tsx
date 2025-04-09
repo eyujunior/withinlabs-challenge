f69ab453128d33979e87ad34029b4e857a73a8a2
eyujunior <eyujunior052@gmail.com>
2025-04-09 17:02:40 +0300

    fixed missing width and height

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -95,6 +95,8 @@ export function Cart() {
                                                         <li key={item.product.id} className="py-6 flex">
                                                             <div className="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden">
                                                                 <Image
+                                                                    width={600}
+                                                                    height={600}
                                                                     src={item.product.thumbnail}
                                                                     alt={item.product.title}
                                                                     className="w-full h-full object-cover"
